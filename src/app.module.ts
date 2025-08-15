ca1d067a3c4911650446047c13167425fc77b3f2
daniel-idowu-01 <danielidowu414@gmail.com>
2025-08-15 22:04:40 +0100

    feat: TASKS flow

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { TeamsModule } from './teams/teams.module';
 import { ProjectsModule } from './projects/projects.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [UserModule, AuthModule, TeamsModule, ProjectsModule],
+  imports: [UserModule, AuthModule, TeamsModule, ProjectsModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
