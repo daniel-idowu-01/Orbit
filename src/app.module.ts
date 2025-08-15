e77e597663fac9c5a93fe770a3c39cf22aa6909d
daniel-idowu-01 <danielidowu414@gmail.com>
2025-08-15 22:03:19 +0100

    feat: PROJECTS FLOW

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { TeamsModule } from './teams/teams.module';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
-  imports: [UserModule, AuthModule, TeamsModule],
+  imports: [UserModule, AuthModule, TeamsModule, ProjectsModule],
   controllers: [AppController],
   providers: [AppService],
 })
