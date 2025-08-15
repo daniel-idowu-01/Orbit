541d7baf2be6bee1546eae1150313362fdecadf8
daniel-idowu-01 <danielidowu414@gmail.com>
2025-08-15 22:01:32 +0100

    feat: add Teams module, controller, and service; update dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { TeamsModule } from './teams/teams.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, TeamsModule],
   controllers: [AppController],
   providers: [AppService],
 })
