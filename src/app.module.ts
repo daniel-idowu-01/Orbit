158fac8a74a4be2512a3d87af7bcf47b1cea6cfa
daniel-idowu-01 <danielidowu414@gmail.com>
2025-08-15 22:06:01 +0100

    feat: COMMENTS flow

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { AuthModule } from './auth/auth.module';
 import { TeamsModule } from './teams/teams.module';
 import { ProjectsModule } from './projects/projects.module';
 import { TasksModule } from './tasks/tasks.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [UserModule, AuthModule, TeamsModule, ProjectsModule, TasksModule],
+  imports: [UserModule, AuthModule, TeamsModule, ProjectsModule, TasksModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
